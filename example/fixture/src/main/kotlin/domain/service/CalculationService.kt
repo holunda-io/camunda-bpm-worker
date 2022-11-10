3c26bc8c9cfb9640a87f21013cd6a72eb3621c84
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-11-10 23:03:09 +0100

    add definitions

--- example/fixture/src/main/kotlin/domain/service/CalculationService.kt
+++ example/fixture/src/main/kotlin/domain/service/CalculationService.kt
@@ -5,7 +5,6 @@ import org.jmolecules.architecture.onion.classical.DomainServiceRing
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 
-@DomainServiceRing
 @Component
 class CalculationService {
 
